93694168111f36ae03d09ff25f4a531f0e1eaf4f
Iñigo <igargi21@hotmail.com>
2017-03-14 10:28:48 +0100

    Estilizado de la página de contacto y traducciones

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
 import { AppInit } from '../../providers/app-init';
-import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
-
-
+import { GoogleMap } from 'ionic-native';
 
 declare var google;
 
@@ -12,17 +10,16 @@ declare var google;
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+/*Iniciamos el mapa*/
  map: GoogleMap;
   constructor(public navCtrl: NavController, public navParams: NavParams, private AppInit: AppInit,private platform:Platform) {
     this.platform = platform;
-
-		this.initializeMap();
+		this.initializeMap(); //Cuando esté cargado, inicializamos el mapa
      }
      initializeMap() {
 
-		this.platform.ready().then(() => {
+		this.platform.ready().then(() => { //Definimos las variables del mapa
 		    var minZoomLevel = 17;
-
 		    this.map = new google.maps.Map(document.getElementById('map'), {
 		        zoom: minZoomLevel,
 		        center: new google.maps.LatLng(42.800783,-1.63668),
