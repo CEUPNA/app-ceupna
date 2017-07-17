bc098c65084f6978f64e62979cb9e1fc7fde3aa4
Iñigo <igargi21@hotmail.com>
2017-07-18 00:21:44 +0200

    Añadir traducciones

--- src/pages/complaints/complaints.ts
+++ src/pages/complaints/complaints.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AppInit } from '../../providers/app-init';
+import { TranslateService } from 'ng2-translate';
 
 
 @Component({
